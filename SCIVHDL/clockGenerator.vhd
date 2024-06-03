7ea9b4e522ed78f8d54599a5ca81803c3637df87
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-03 10:34:50 -0400

    clock divider commented

--- SCIVHDL/clockGenerator.vhd
+++ SCIVHDL/clockGenerator.vhd
@@ -1,18 +1,27 @@
+--
+--clockGenerator.vhd --- clock didider divides 100Mhz system clock down to 1MHz.
+-- Used clock divider from lab 5 as reference.
+--Author: Joshua Meise and Brandon Zhao
+--Created: 05-26-2024
+--
+
+-- Library inclusions.
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.math_real.all;
-
 library UNISIM;
 use UNISIM.VComponents.all;
 
+-- entity definition.
 entity clockGenerator is
   port (clkExtPort: in std_logic;
         clkPort: out std_logic);
 end clockGenerator;
 
+-- Architecture definition.
 architecture behavioral of clockGenerator is
-
+  -- Constants and internal signals.
   constant clkDividerTC: integer := 50;
   constant countLen: integer := integer(ceil(log2(real(clkDividerTC))));
   signal clkDividerCtr: unsigned(countLen-1 downto 0) := (others => '0');
@@ -21,8 +30,9 @@ architecture behavioral of clockGenerator is
 begin
   clockDivider: process(clkExtPort)
   begin
-  -- Slow down 100 times to 1 MHz
     if rising_edge(clkExtPort) then
+      -- Toggle the slowed clock when we reach 49 clock cycles and clear the
+      -- counter, otherwise keep counting the number of clock cycles.
       if clkDividerCtr = clkDividerTC - 1 then
         clkTog <= not(clkTog);
         clkDividerCtr <= (others => '0');
@@ -32,6 +42,7 @@ begin
     end if;
   end process;
 
+  -- Add in a buffer.
   clkBuffer: BUFG
     port map (I => clkTog,
               O => clkPort);
