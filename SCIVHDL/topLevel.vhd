ad27e7565eed5f680a0fd728fde147f83f6ca295
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-04 14:32:28 -0400

    toplevel updated such that trans is no longer necessary

--- SCIVHDL/topLevel.vhd
+++ SCIVHDL/topLevel.vhd
@@ -116,29 +116,41 @@ architecture structural of calculator is
           regPort: out regType);
   end component;
 
-  -- FIFO and transmitter.
-  component trans is
+  -- FIFO
+  component toOutput is
     port (clk: in std_logic;
-          numRegPort: in regType;
-          newNumRegPort: in std_logic;
-          opRegPort: in regType;
-          newOpRegPort: in std_logic;
-          ansRegPort: regType;
-          newAnsRegPort: in std_logic;
-          numMaxAddrPort: in unsigned(7 downto 0);
-          opMaxAddrPort: in unsigned(7 downto 0);
-          ansMaxAddrPort: in unsigned(7 downto 0);
-          TxPort: out std_logic;
-          TCDonePort: out std_logic);
-  end component;
+          TxReady: in std_logic;
+          numReg: in regType;
+          newNumReg: in std_logic;
+          opReg: in regType;
+          newOpreg: in std_logic;
+          ansReg: in regType;
+          newAnsReg: in std_logic;
+          numMaxAddr: in unsigned(7 downto 0);
+          opMaxAddr: in unsigned(7 downto 0);
+          ansMaxAddr: in unsigned(7 downto 0);  
+          data: out std_logic_vector(7 downto 0);
+          newData: out std_logic;
+          TCdone: out std_logic);
+	end component; 
 
-  -- INternal signals.
-  signal clk, RXDone, ansSend, ASend, BSend, opSend, resetEn, AEn, BEn, sumEn, multEn, subEn, calcEn, newNum, newNumReg, newOpReg, newAnsReg, newReg, TCDone, loadSig, addSig, subSig, multSig, resetSig: std_logic := '0';
+  -- Transmitter.
+  component transmitter is
+    port (clk: in std_logic;
+          data: in std_logic_vector(7 downto 0);
+      	  newData: in std_logic;
+      	  Tx: out std_logic;
+      	  TxReady: out std_logic);
+	end component;
+
+  -- Internal signals.
+  signal clk, RXDone, ansSend, ASend, BSend, opSend, resetEn, AEn, BEn, sumEn, multEn, subEn, calcEn, newNum, newNumReg, newOpReg, newAnsReg, newReg, TCDone, loadSig, addSig, subSig, multSig, resetSig, TxReady, newData: std_logic := '0';
   signal A, B, ans: signed(15 downto 0) := (others => '0');
   signal op: opType := sum;
   signal numMaxAddr, opMaxAddr, ansMaxAddr, maxAddr: unsigned(7 downto 0) := (others => '0');
   signal numReg, opReg, ansReg, reg: regType := (others => (others => '0'));
   signal num: signed(7 downto 0) := (others => '0');
+  signal data: std_logic_vector(7 downto 0) := (others => '0');
 
 begin
   
@@ -227,17 +239,27 @@ begin
               newRegPort => newAnsReg,
               regport => ansReg);
 
-  transmitter: trans
-    port map(clk => clk,
-             numRegPort => numReg,
-             newNumRegPort => newNumReg,
-             opRegPort => opReg,
-             newOpRegPort => newOpReg,
-             ansRegPort => ansReg,
-             newAnsRegPort => newAnsReg,
-             numMaxAddrPort => numMaxAddr,
-             opMaxAddrPort => opMaxAddr,
-             ansMaxAddrPort => ansMaxAddr,
-             TxPort => TXExtPort,
-             TCDonePort => TCDone);
+  toOut: toOutput
+    port map (clk => clk,
+              TxReady => TxReady,
+              numReg => numReg,
+              newNumReg => newNumReg,
+              opReg => opReg,
+              newOpReg => newOpReg,
+              ansReg => ansReg,
+              newAnsReg => newAnsReg,
+              numMaxAddr => numMaxAddr,
+              opMaxAddr => opMaxAddr,
+              ansMaxAddr => ansMaxAddr,
+              data => data,
+              newData => newData,
+              TCDone => TCDone);
+
+  trans: transmitter
+    port map (clk => clk,
+              data => data,
+              newData => newData,
+              Tx => TxExtPort,
+              TxReady => TxReady);
+  
 end structural;
