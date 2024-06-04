f33405f964e4aae7536f87856a38c6c4c4eb8bb4
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-04 00:08:06 -0400

    receiver testbench modified

--- SCIVHDL/receiver_tb.vhd
+++ SCIVHDL/receiver_tb.vhd
@@ -1,33 +1,39 @@
--- Code your testbench here
+--
+--receiver_tb.vhd --- testbench for receiver block.
+--
+--Author: Joshua Meise and Brandon Zhao
+--
+
+-- Library inclusions.
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-
 library work;
 use work.mypackage.all;
 
+-- Enttity definition.
 entity receiver_tb is
 end receiver_tb;
 
+-- Architecture definition.
 architecture testbench of receiver_tb is
-
 	component receiver is
     port (clk: in std_logic;
-          Rx: in std_logic;
-          mat: out matrix;
-          matReadyPort: out std_logic);
+          RxPort: in std_logic;
+          numPort: out signed(7 downto 0);
+          RxDonePort: out std_logic);
 	end component;
   
   signal clk: std_logic := '0';
-  signal Rx, matReady: std_logic;
-  signal mat: matrix;
+  signal Rx, RxDonePort: std_logic := '0';
+  signal numPort: signed(7 downto 0) := (others => '0');
   
 begin
 	uut: receiver
     port map (clk => clk,
-              Rx => Rx,
-              mat => mat,
-              matReadyPort => matReady);
+              RxPort => Rx,
+              numPort => numPort,
+              RxDonePort => RxDonePort);
     
   clock: process
   begin
@@ -208,15 +214,15 @@ begin
     wait for 1us;
     Rx <= '0';
     wait for 1us;
-    Rx <= '1';
+    Rx <= '0';
+    wait for 1us;
+    Rx <= '0';
+    wait for 1us;
+    Rx <= '0';
     wait for 1us;
     Rx <= '0';
     wait for 1us;
     Rx <= '1';
-    wait for 1us;
-    Rx <= '0';
-    wait for 1us;
-    Rx <= '0';
     wait for 1us;
     Rx <= '0';
     wait for 1us;
